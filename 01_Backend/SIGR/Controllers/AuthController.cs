3804375faf7d599019e33b19dcfabf80d5c82fca
Juan Acevedo <juanfdoa@hotmail.com>
2026-05-11 21:44:14 -0500

    feat: implementar módulo de autenticación con JWT

---
+++ 01_Backend/SIGR/Controllers/AuthController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIGR.Controllers
+{
+    [Controller]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("register")]
+        public IActionResult Register([FromBody] RegisterDto dto)
+        {
+            return Ok(dto);
+        }
+
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginDto dto)
+        {
+            var user = new { dto.Email, dto.Password };
+            return Ok(user);
+        }
+
+        public class RegisterDto
+        {
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+
+        public class LoginDto
+        {
+            public string Email { get; set; }
+            public string Password { get; set; }
+        }
+    }
+}
