3fd6f1b2b058cee70e4f47c6998fc4b50e05fa42
Juan Acevedo <juanfdoa@hotmail.com>
2026-05-25 12:52:38 -0500

    add reservation controller

---
+++ 01_Backend/SIGR/Controllers/ReservationController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIGR.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservationController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetReservations()
+        {
+            return Ok(new[]
+            {
+                new {
+                    Id = 1,
+                    Customer = "Juan",
+                    Table = 5
+                }
+            });
+        }
+    }
+}
